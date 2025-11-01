bbee39ff8827d643d38cafaa3e379675cdd2f7a4
romanornr <romanornr@gmail.com>
2025-11-01 19:29:21 +0100

    feat: wallet EVM selection

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { Layer } from "@/services/config";
 import { getPreferredWeb3ProviderAsync } from "@/utils/ethereum-provider";
 import { useMobile } from "@/hooks/use-mobile";
+import { WalletConnectButton as EVMSelectorButton } from "@/components/wallets/connect-button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -182,11 +183,8 @@ export default function Header() {
           )}
         </>
       ) : (
-        <DropdownMenuItem 
-          onClick={handleConnectMetamask}
-          disabled={isConnectingMetaMask}
-        >
-          {isConnectingMetaMask ? "Connecting..." : "Connect EVM Wallet"}
+        <DropdownMenuItem asChild>
+          <EVMSelectorButton />
         </DropdownMenuItem>
       )}
     </>
@@ -259,14 +257,15 @@ export default function Header() {
                   <span>VIA: {viaAddress?.slice(0, 6)}...{viaAddress?.slice(-4)}</span>
                 </Button>
               ) : (
-                <Button 
-                  variant="outline" 
-                  size="sm"
-                  onClick={handleConnectMetamask}
-                  disabled={isConnectingMetaMask}
-                >
-                  {isConnectingMetaMask ? "Connecting..." : "Connect EVM Wallet"}
-                </Button>
+                <EVMSelectorButton />
+                // <Button
+                //   variant="outline"
+                //   size="sm"
+                //   onClick={handleConnectMetamask}
+                //   disabled={isConnectingMetaMask}
+                // >
+                //   {isConnectingMetaMask ? "Connecting..." : "Connect EVM Wallet"}
+                // </Button>
               )}
             </div>
           )}
